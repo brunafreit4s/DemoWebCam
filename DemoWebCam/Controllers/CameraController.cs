45046bb63e97902d0d51b309a924a3e8963e3f2e
brunafreit4s <bubufreitas@hotmail.com>
2022-03-14 20:06:39 -0300

    Adicionando métodos de captura para armazenar no banco de dados

--- DemoWebCam/Controllers/CameraController.cs
+++ DemoWebCam/Controllers/CameraController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,89 @@ namespace DemoWebCam.Controllers
 {
     public class CameraController : Controller
     {
-        public IActionResult Capture()
+        [HttpPost]
+        public IActionResult Capture(string name)
         {
-            return View();
+            var files = HttpContext.Request.Form.Files;
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        // Getting Filename  
+                        var fileName = file.FileName;
+                        // Unique filename "Guid"  
+                        var myUniqueFileName = Convert.ToString(Guid.NewGuid());
+                        // Getting Extension  
+                        var fileExtension = Path.GetExtension(fileName);
+                        // Concating filename + fileExtension (unique filename)  
+                        var newFileName = string.Concat(myUniqueFileName, fileExtension);
+                        //  Generating Path to store photo   
+                        var filepath = Path.Combine(_environment.WebRootPath, "CameraPhotos") + $@"\{newFileName}";
+
+                        if (!string.IsNullOrEmpty(filepath))
+                        {
+                            // Storing Image in Folder  
+                            StoreInFolder(file, filepath);
+                        }
+
+                        var imageBytes = System.IO.File.ReadAllBytes(filepath);
+                        if (imageBytes != null)
+                        {
+                            // Storing Image in Folder  
+                            StoreInDatabase(imageBytes);
+                        }
+
+                    }
+                }
+                return Json(true);
+            }
+            else
+            {
+                return Json(false);
+            }
+        }
+
+        /// <summary>  
+        /// Saving captured image into Folder.  
+        /// </summary>  
+        /// <param name="file"></param>  
+        /// <param name="fileName"></param>  
+        private void StoreInFolder(IFormFile file, string fileName)
+        {
+            using (FileStream fs = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fs);
+                fs.Flush();
+            }
+        }
+
+        /// <summary>  
+        /// Saving captured image into database.  
+        /// </summary>  
+        /// <param name="imageBytes"></param>  
+        private void StoreInDatabase(byte[] imageBytes)
+        {
+            try
+            {
+                if (imageBytes != null)
+                {
+                    string base64String = Convert.ToBase64String(imageBytes, 0, imageBytes.Length);
+                    string imageUrl = string.Concat("data:image/jpg;base64,", base64String);
+                    ImageStore imageStore = new ImageStore()
+                    {
+                        CreateDate = DateTime.Now,
+                        ImageBase64String = imageUrl,
+                        ImageId = 0
+                    };
+                    _context.ImageStore.Add(imageStore);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
-}
