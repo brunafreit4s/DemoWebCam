8e85688c593b12b51068ef2d155f3f51ea80c941
brunafreit4s <bubufreitas@hotmail.com>
2022-03-14 17:38:58 -0300

    Add project files.

---
+++ DemoWebCam/Controllers/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebCam.Controllers
+{
+    public class CameraController : Controller
+    {
+        public IActionResult Capture()
+        {
+            return View();
+        }
+    }
+}
